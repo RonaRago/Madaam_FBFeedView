2e2178be060ac43d1c1bd1e2ab29f8e4948e019e
IOS DEV <IOS.DEV@mail.com>
2018-08-15 09:52:22 +0800

    first commit

---
+++ Mdaam_FBFeed/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Mdaam_FBFeed
+//
+//  Created by Rona Rago on 14/08/2018.
+//  Copyright © 2018 Rona Rago. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+}
+
