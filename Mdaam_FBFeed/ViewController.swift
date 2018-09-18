7ccc3f3abb198f34f31afa817f0b0bd6da415772
IOS DEV <IOS.DEV@mail.com>
2018-09-18 15:01:15 +0800

    added new user and post

--- Mdaam_FBFeed/ViewController.swift
+++ Mdaam_FBFeed/ViewController.swift
@@ -15,6 +15,9 @@ class Post{
     var name: String?
     var statusText: String?
     var profileImageName: String?
+    var statusImageName: String?
+    var numLikes: Int?
+    var numComments: Int?
 }
 class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
@@ -26,20 +29,39 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     override func viewDidLoad() {
     super.viewDidLoad()
         
-        let postMark = Post()
-        postMark.name = "Mark Zuckerberg"
-        postMark.statusText = "Meanwhile Beast turned to the darkside"
-        postMark.profileImageName = "zuckprofile"
+                let postMark = Post()
+                postMark.name = "Mark Zuckerberg"
+                postMark.profileImageName = "zuckprofile"
+                postMark.statusText = "By giving people the power to share, we're making the world more transparent."
+                postMark.statusImageName = "zuckdog"
+                postMark.numLikes = 400
+                postMark.numComments = 123
         
-        let postSteve = Post()
-        postSteve.name = "Stve Jobs"
-        postSteve.statusText = "Design is not just what it looks like and feels like. Design is how it works.\n\n" +
-                        "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me.\n\n" +
-                        "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
-        postSteve.profileImageName = "steve_profile"
+                let postSteve = Post()
+                postSteve.name = "Steve Jobs"
+                postSteve.profileImageName = "steve_profile"
+                postSteve.statusText = "Design is not just what it looks like and feels like. Design is how it works.\n\n" +
+                    "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me.\n\n" +
+                "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
+                postSteve.statusImageName = "steve_status"
+                postSteve.numLikes = 1000
+                postSteve.numComments = 55
+        
+                let postGandhi = Post()
+                postGandhi.name = "Mahatma Gandhi"
+                postGandhi.profileImageName = "gandhi_profile"
+                postGandhi.statusText = "Live as if you were to die tomorrow; learn as if you were to live forever.\n" +
+                    "The weak can never forgive. Forgiveness is the attribute of the strong.\n" +
+                "Happiness is when what you think, what you say, and what you do are in harmony."
+                postGandhi.statusImageName = "gandhi_status"
+                postGandhi.numLikes = 333
+                postGandhi.numComments = 22
+
         
         posts.append(postMark)
         posts.append(postSteve)
+        posts.append(postGandhi)
+        
 
     //Navigationbar Title
     navigationItem.title = "Facebook Feed"
