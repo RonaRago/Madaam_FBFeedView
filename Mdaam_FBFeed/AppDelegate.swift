a99dea7bdbda983f2d698404840f49c1c68d9299
IOS DEV <IOS.DEV@mail.com>
2018-09-18 16:38:46 +0800

    change default color of TAB ICON

--- Mdaam_FBFeed/AppDelegate.swift
+++ Mdaam_FBFeed/AppDelegate.swift
@@ -22,6 +22,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
         UINavigationBar.appearance().barTintColor = UIColor(red: 51/255, green: 90/255, blue: 149/255, alpha: 1)
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+    
+        //default color for Tab Icon
+        UITabBar.appearance().tintColor = UIColor.rgb(70, green: 146, blue: 250)
+    
         application.statusBarStyle = .lightContent
     
         // Override point for customization after application launch.
