2cd3d81eda53537ab59f569ea92d66db5a27bf74
IOS DEV <IOS.DEV@mail.com>
2018-09-18 16:31:43 +0800

    change height and color of horizontal div

--- Mdaam_FBFeed/CustomTabBarController.swift
+++ Mdaam_FBFeed/CustomTabBarController.swift
@@ -10,39 +10,49 @@ import UIKit
 
 class CustomTabBarController : UITabBarController{
     override func viewDidLoad(){
-    super.viewDidLoad()
-                let feedController = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
-                let navigationController = UINavigationController(rootViewController: feedController)
-                //First tab  News Feed
-                navigationController.title = "News Feed"
-                navigationController.tabBarItem.image = UIImage(named: "news_feed_icon")
+        super.viewDidLoad()
+            let feedController = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
+            let navigationController = UINavigationController(rootViewController: feedController)
+            //First tab  News Feed
+            navigationController.title = "News Feed"
+            navigationController.tabBarItem.image = UIImage(named: "news_feed_icon")
+
+            //Second tab  Friend Request
+            let friendRequestsController = FriendRequestsController()
+            let secondnavigationController = UINavigationController(rootViewController: friendRequestsController)
+            secondnavigationController.title = "Requests"
+            secondnavigationController.tabBarItem.image = UIImage(named: "requests_icon")
+
+            //Third tab  Messenger
+            let messengerVC = UIViewController()
+            messengerVC.navigationItem.title = "Messenger View"
+            let messengernavigationController = UINavigationController(rootViewController: messengerVC)
+            messengernavigationController.title = "Messenger"
+            messengernavigationController.tabBarItem.image = UIImage(named: "messenger_icon")
+
+            //Fourth tab  Global Notifications
+            let notifVC = UIViewController()
+            notifVC.navigationItem.title = "Global Notifications"
+            let notificationsnavigationController = UINavigationController(rootViewController: notifVC)
+            notificationsnavigationController.title = "Notifications"
+            notificationsnavigationController.tabBarItem.image = UIImage(named: "globe_icon")
+
+            //Fifth tab  Messenger
+            let moreVC = UIViewController()
+            moreVC.navigationItem.title = "More View"
+            let morenavigationController = UINavigationController(rootViewController: moreVC)
+            morenavigationController.title = "More"
+            morenavigationController.tabBarItem.image = UIImage(named: "more_icon")
         
-                //Second tab  Friend Request
-                let friendRequestsController = FriendRequestsController()
-                let secondnavigationController = UINavigationController(rootViewController: friendRequestsController)
-                secondnavigationController.title = "Requests"
-                secondnavigationController.tabBarItem.image = UIImage(named: "requests_icon")
+            tabBar.isTranslucent = false
         
-                //Third tab  Messenger
-                let messengerVC = UIViewController()
-                messengerVC.navigationItem.title = "Messenger View"
-                let messengernavigationController = UINavigationController(rootViewController: messengerVC)
-                messengernavigationController.title = "Messenger"
-                messengernavigationController.tabBarItem.image = UIImage(named: "messenger_icon")
+            //change height and color of horizontal div
+            let topBorder = CALayer()
+            topBorder.frame = CGRect(x: 0, y: 0, width: 1000, height: 0.5)
+            topBorder.backgroundColor = UIColor.rgb(229, green: 231, blue: 235).cgColor
         
-                //Fourth tab  Global Notifications
-                let notifVC = UIViewController()
-                notifVC.navigationItem.title = "Global Notifications"
-                let notificationsnavigationController = UINavigationController(rootViewController: notifVC)
-                notificationsnavigationController.title = "Notifications"
-                notificationsnavigationController.tabBarItem.image = UIImage(named: "globe_icon")
-        
-                //Fifth tab  Messenger
-                let moreVC = UIViewController()
-                moreVC.navigationItem.title = "More View"
-                let morenavigationController = UINavigationController(rootViewController: moreVC)
-                morenavigationController.title = "More"
-                morenavigationController.tabBarItem.image = UIImage(named: "more_icon")
+            tabBar.layer.addSublayer(topBorder)
+            tabBar.clipsToBounds = true
         
         
         
